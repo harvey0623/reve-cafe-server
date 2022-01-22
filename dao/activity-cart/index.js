48d5fad3545d26e63ea7ad5aada2e5becc90743b
harvey <harvey@henghung.com>
2022-01-22 15:06:43 +0800

    add api

--- dao/activity-cart/index.js
+++ dao/activity-cart/index.js
@@ -11,6 +11,17 @@ const activityCartDao = {
          return err.response.data;
       })
    },
+   addCart(payload) {
+      return mmrmAxios({
+         url: '/api/activity_cart/product_promotions/bundle',
+         method: 'post',
+         data: payload
+      }).then(res => {
+         return res.data; 
+      }).catch(err => {
+         return err.response.data;
+      })
+   },
 };
 
 module.exports = activityCartDao;
--- route/activity-cart/index.js
+++ route/activity-cart/index.js
@@ -3,12 +3,19 @@ const router = express.Router();
 const activityCartDao = require('../../dao/activity-cart/index.js');
 const tempSave = require('../../utility/tempSave/index.js');
 
-
 router.get('/product_promotions/bundle', async(req, res) => {
    tempSave.access_token = req.headers.authorization;
    let response = await activityCartDao.getCart(req.body);
    let statusCode = response.status === 1 ? 200 : 401;
    res.status(statusCode).json(response);
 });
+
+router.post('/product_promotions/bundle', async(req, res) => {
+   tempSave.access_token = req.headers.authorization;
+   let response = await activityCartDao.getCart(req.body);
+   let statusCode = response.status === 1 ? 200 : 401;
+   res.status(statusCode).json(response);
+});
+
 
 module.exports = router;
